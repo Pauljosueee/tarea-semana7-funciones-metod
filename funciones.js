c45dd1a904f5fbc5b931588de8e7ad464f89e772
Pauljosueee <166031203+Pauljosueee@users.noreply.github.com>
2024-05-15 21:33:39 -0500

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,23 @@
+'use strict'
+
+//Sin parametros, sin valor de retorno
+function mostrarMensaje(){
+    alert('Adios'+ mostrarMensaje);
+}
+
+//Sin parametros, con valor de retorno 
+function GenerarFrase (){
+  return "Sudamericano"; 
+}
+alert (GenerarFrase ());
+
+//Con parametros, con valor de retorno
+
+function sumar(a,b){
+   return(4+1);
+}
+
+//Con parametros, sin valor de retorno
+function mostrarfrase (Paul_Muela){
+    alert (mostrarfrase);
+}
